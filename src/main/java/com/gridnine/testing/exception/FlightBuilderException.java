6f6618b1a3622fb847358ee285a8dc58a5e73fcf
matsukata <69299734+matsukata@users.noreply.github.com>
2021-06-22 19:16:39 +0300

    feature/ add-exception (#3)
    add exception;

---
+++ src/main/java/com/gridnine/testing/exception/FlightBuilderException.java
@@ -0,0 +1,7 @@
+package com.gridnine.testing.exception;
+
+public class FlightBuilderException extends IllegalArgumentException {
+    public FlightBuilderException(String message) {
+        super(message);
+    }
+}
